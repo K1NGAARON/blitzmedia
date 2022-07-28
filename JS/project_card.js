f31bf981a8014bdb841675a64b27cbc16a4db4f8
K1NGAARON <gerardaaron1997@gmail.com>
2022-07-28 12:27:08 +0200

    layout script

--- JS/project_card.js
+++ JS/project_card.js
@@ -1,1 +1,31 @@
-const projectWrapper = document.querySelector('.project-wrapper');
+const projectHolder = document.querySelector('#our-projects');
+
+// ARRAY WITH PROJECTS
+const projectShowcase = [
+    {
+        'img' : '',
+        'title' : '',
+        'intro' : '',
+        'url' : ''
+    },
+    {
+        'img' : '',
+        'title' : '',
+        'intro' : '',
+        'url' : ''
+    },
+    {
+        'img' : '',
+        'title' : '',
+        'intro' : '',
+        'url' : ''
+    }
+]
+
+
+
+// CREATE PROJECTPOST
+
+// APPEND TO PROJECTHOLDER
+
+// CREATE SLIDER FUNCTION
