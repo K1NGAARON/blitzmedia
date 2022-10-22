bfeb2fffd692c7a269776ad714f481c1bfe23ed3
K1NGAARON <gerardaaron1997@gmail.com>
2022-10-22 17:51:52 +0200

    changes

--- JS/faq.js
+++ JS/faq.js
@@ -1,32 +1,5 @@
-$( document ).ready(function() {
-var Accordion = function(el, multiple) {
-    this.el = el || {};
-    this.multiple = multiple || false;
-
-    var links = this.el.find('.article-title');
-    links.on('click', {
-            el: this.el,
-            multiple: this.multiple
-    }, this.dropdown)
-}
-
-Accordion.prototype.dropdown = function(e) {
-    var $el = e.data.el;
-    $this = $(this),
-        $next = $(this).next();
-
-    $next.slideToggle();
-    $(this).parent().toggleClass('open');
-
-    if (!e.data.multiple) {
-        $el.find('.accordion-content').not($next).slideUp().parent().removeClass('open');
-    };
-}
-var accordion = new Accordion($('.accordion-container'), false);
-
-$(document).on('click', function (event) {
-    if (!$(event.target).closest('#accordion').length) {
-        $(this).parent().toggleClass('open');
-    }
-    });
-})
+$('.accordion-item').click(function() {
+    $(this).children('.accordion-head').toggleClass('active');
+    $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
+    $(this).children('.accordion-head').children('.icon').toggleClass('active');
+});
