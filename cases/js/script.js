b1326112809ceb3d1d6e19df96262d87970487fb
K1NGAARON <gerardaaron1997@gmail.com>
2022-10-30 16:17:21 +0100

    cases overview page

--- cases/js/script.js
+++ cases/js/script.js
@@ -1,3 +1,7 @@
+function filterContent(e) {
+    
+}
+
 $('.filter-wrapper a').on('click', function(e) {
     e.preventDefault();
 
@@ -11,25 +15,38 @@ $('.filter-wrapper a').on('click', function(e) {
 const casesTarget = document.querySelector('#content > .row');
 const casesContent = [
     {
-        'title' : '',
-        'intro' : '',
-        'img' : '',
-        'postLink' : '',
-        'category' : '',
+        'title' : 'Content Media website revamp',
+        'intro' : 'Lorem ipsum, dolor sit amet consectetur adipisicing.',
+        'img' : 'https://blitz-media.io/IMG/aaron_gerard.jpg',
+        'postLink' : 'google.com',
+        'category' : 'webdev',
     },
     {
-        'title' : '',
-        'intro' : '',
-        'img' : '',
-        'postLink' : '',
-        'category' : '',
+        'title' : 'The B2B paid ads strategy for Sunday',
+        'intro' : 'Lorem ipsum, dolor sit amet consectetur adipisicing.',
+        'img' : 'https://blitz-media.io/IMG/aaron_gerard.jpg',
+        'postLink' : 'google.com',
+        'category' : 'advertising',
     }
 ];
 
 function createCasesListing() {
     for (let i = 0; i < casesContent.length; i++) {
         const casesTemplate = `
-
+        <div class="case-wrapper ${casesContent[i].category}">
+            <img src="${casesContent[i].img}" alt="${casesContent[i].title}">
+            <div class="content">
+                <h3>
+                    ${casesContent[i].title}
+                </h3>
+                <p>
+                    ${casesContent[i].intro}
+                </p>
+                <a href="${casesContent[i].postLink}">
+                    Read more
+                </a>
+            </div>
+        </div>
         `;
         casesTarget.insertAdjacentHTML("afterbegin", casesTemplate);
     };
