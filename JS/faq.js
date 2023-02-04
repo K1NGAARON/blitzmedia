64d3b0334ba84d3a38b8fff4fde981dbc84b864b
K1NGAARON <gerardaaron1997@gmail.com>
2023-02-04 19:19:09 +0100

    changes

--- JS/faq.js
+++
@@ -1,5 +0,0 @@
-$('.accordion-item').click(function() {
-    $(this).children('.accordion-head').toggleClass('active');
-    $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
-    $(this).children('.accordion-head').children('.icon').toggleClass('active');
-});
