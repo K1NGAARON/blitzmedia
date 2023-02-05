f08366489c491192175dd3ce91b7e48dfd0fad82
K1NGAARON <gerardaaron1997@gmail.com>
2023-02-05 15:33:16 +0100

    added related content JS CSS and implemented it in the portfolio page

---
+++ portfolio/js/script.js
@@ -0,0 +1,22 @@
+const content = [
+    {
+        title: "Title 1",
+        body: "Body 1",
+        img: "/portfolio/img/placeholder.jpg",
+        url: "/portfolio/",
+    },
+    {
+        title: "Title 2",
+        body: "Body 2",
+        img: "/portfolio/img/placeholder.jpg",
+        url: "/portfolio/",
+    },
+    {
+        title: "Title 3",
+        body: "Body 3",
+        img: "/portfolio/img/placeholder.jpg",
+        url: "/portfolio/",
+    },
+];
+
+const portfolioTarget = document.querySelector('#content');
