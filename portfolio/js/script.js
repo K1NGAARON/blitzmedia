0143eed156af388d008f5be84d2b709370489ea1
K1NGAARON <gerardaaron1997&gmail.com>
2023-02-17 14:30:33 +0100

    deploy portoflio page

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -2,13 +2,13 @@ const content = [
     {
         title: "Hoe we 150+ leads gegenereerd hebben voor een boekhouder",
         body: "De website bezoekers van Freedom Academy hadden geen flauw idee wat het bedrijf net deed.",
-        img: "/portfolio/img/placeholder.jpg",
+        img: "/portfolio/boekhouder/img/banner.jpg",
         url: "/portfolio/boekhouder/",
     },
     {
         title: "Hoe de Freedom Academy bezoekers converteert naar betalende coaching klanten",
         body: "De uitdaging bij deze boekhouder was dat er geen manier was om voorspelbaar meer leads (lees: klanten) aan te trekken.",
-        img: "/portfolio/img/placeholder.jpg",
+        img: "/portfolio/freedom-academy/img/banner.jpg",
         url: "/portfolio/freedom-academy/",
     },
 ];
